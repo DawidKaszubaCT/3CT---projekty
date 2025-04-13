dc53e2d96d3e595d4f25c37fdd3daffa6319cb6a
DawidKaszubaCT <144652698+DawidKaszubaCT@users.noreply.github.com>
2025-04-13 19:37:50 +0200

    Autor, galeria, kalkulator (C#)

---
+++ AGK/AGK/Form3.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace AGK
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.Location = new Point(51, 115);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(186, 133);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
+            pictureBox2.Location = new Point(303, 115);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(186, 133);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.Location = new Point(566, 115);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(186, 133);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonShadow;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form3";
+            Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+    }
+}
---
+++ AGK/AGK/Form4.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AGK
+{
+    public partial class Form4 : Form
+    {
+        float wynik;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            label1.Text = string.Empty;
+            var wyrazenie = textBox1.Text;
+            var wyrazenieTablica = wyrazenie.ToArray();
+
+            string liczba1 = "";
+            string liczba2 = "";
+            char znak = 'n';
+            foreach( var wy in wyrazenieTablica)
+            {
+
+               if(wy == '+' ||  wy == '-' || wy == '*')
+                {
+                    if(znak == '+')
+                    {
+                        int l1 = int.Parse(liczba1);
+                        int l2 = int.Parse(liczba2);
+                        l1 = l1 + l2;
+                        liczba1 = l1.ToString();
+                        liczba2 = "";
+                    }
+                    if (znak == '-')
+                    {
+                        int l1 = int.Parse(liczba1);
+                        int l2 = int.Parse(liczba2);
+                        l1 = l1 - l2;
+                        liczba1 = l1.ToString();
+                        liczba2 = "";
+                    }
+                    if (znak == '*')
+                    {
+                        int l1 = int.Parse(liczba1);
+                        int l2 = int.Parse(liczba2);
+                        l1 = l1 * l2;
+                        liczba1 = l1.ToString();
+                        liczba2 = "";
+                    }
+                            znak = wy;
+                }
+                else if(znak != 'n')
+                {
+                    liczba2 += wy;
+                }
+                else
+                {
+                    liczba1 += wy;
+                }
+            }
+            if(znak == '+')
+            {
+               wynik = int.Parse(liczba1) + int.Parse(liczba2);
+               znak = 'n';
+            }
+            if(znak == '-')
+            {
+                wynik = int.Parse(liczba1) - int.Parse(liczba2);
+                znak = 'n';
+            }
+            if (znak == '*')
+            {
+                wynik = int.Parse(liczba1) * int.Parse(liczba2);
+                znak = 'n';
+            }
+
+
+            label1.Text = $"Wynik: {wynik.ToString()}";
+            
+        }
+    }
+}
